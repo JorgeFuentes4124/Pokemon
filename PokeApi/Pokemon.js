3559fb5b84ce8521b9ae0ecaa12d1bfdaae54789
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-08-05 14:57:06 -0300

    Update Pokemon.js

--- PokeApi/Pokemon.js
+++ PokeApi/Pokemon.js
@@ -10,9 +10,7 @@ function solicitudAJAX() {
         console.dir(json);
 
         tarjetas.data = json;
-        for (let i = 0; i < json.results.length; i++) {
-          buscarPorURL(json.results[i].url);
-        }
+          buscarPorURL(json.results[0].url);
       } else {
         alert("Error Code: " + objXMLHttpRequest.status);
         alert("Error Message: " + objXMLHttpRequest.statusText);
